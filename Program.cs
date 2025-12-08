570eb7d96613f7a21519ebccc95c6613cfb1c5a1
Ömer Dilaveroğlu <77552731+omerdilaveroglu@users.noreply.github.com>
2025-12-08 21:55:11 +0300

    username ve password özellikleri eklendi

--- Program.cs
+++ Program.cs
@@ -12,6 +12,21 @@ builder.Services.AddDbContext<IdentityContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>() 
     .AddEntityFrameworkStores<IdentityContext>();
 
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 1;
+
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
